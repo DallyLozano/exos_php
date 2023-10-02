8c3e52218adc2fc7918084ca644dc29f26132408
lozano_chahua_d <lozanochahua-dallymilagros@lycee-jeanrostand.fr>
2023-10-02 15:14:13 +0200

    formulaire de notes: moyenne et media

---
+++ forms_notes/main.php
@@ -0,0 +1,20 @@
+<?php
+
+//include "../ statistique.php";
+
+
+$mynb1 = $_POST['nb1'];
+$mynb2 = $_POST['nb2'];
+$mynb3 = $_POST['nb3'];
+
+if($_POST){
+
+    if(!empty($mynb1) && !empty($mynb2) && !empty($mynb3){
+
+        echo "ok";
+        
+    })
+}
+
+
+?>
